4587e909d680464b2d4a71769c965c84356b397e
wdecoster <wouter.decoster@molgen.vib-ua.be>
2017-06-09 14:48:47 +0200

    initial commit

---
+++ nanosplit/NanoSplit.py
@@ -0,0 +1,51 @@
+#wdecoster
+'''
+Example usage:
+NanoSplit.py -q 10 reads.fastq.gz
+'''
+
+from __future__ import print_function
+from Bio import SeqIO
+import argparse
+import sys
+import gzip
+import os
+from nanomath import aveQual
+from nanoget import handlecompressedFastq
+__version__= "0.1.0"
+
+
+def main():
+    args = getArgs()
+    fqin = handlecompressedFastq(args.fastqfile.name)
+    splitFq(fqin, args)
+
+
+def getArgs():
+    parser = argparse.ArgumentParser(description="Perform splitting of a fastq file based on average basecall quality.")
+    parser.add_argument("-q", "--quality", help="Splitting on this average read quality score", default=12, type=int)
+    parser.add_argument("fastqfile", help="Fastq file to split, can be gz compressed.", type=argparse.FileType('r'))
+    parser.add_argument("--outdir", help="Specify directory in which output has to be created.", default=".")
+    return parser.parse_args()
+
+
+def splitFq(fq, args):
+    '''
+    Split a fastq file in a fail and pass file
+    Optionally trim a number of nucleotides from beginning and end.
+    '''
+    prefix = os.path.join(args.outdir, os.path.basename(args.fastqfile.name).replace('.fastq', '').replace('.gz', '').replace('.fq', ''))
+    p, f = 0, 0
+    with gzip.open(prefix + ".pass.fastq.gz", 'wt') as passed, gzip.open(prefix + ".fail.fastq.gz", 'wt') as failed:
+        for record in SeqIO.parse(fq, "fastq"):
+            if aveQual(record.letter_annotations["phred_quality"]) >= args.quality:
+                p+=1
+                passed.write(record.format("fastq"))
+            else:
+                failed.write(record.format("fastq"))
+                f+=1
+    print("Split the file in {} reads in <pass> and {} reads in <fail>".format(p, f))
+
+
+if __name__ == "__main__":
+    main()
